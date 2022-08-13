d37e44c33bdbda5c4451543273bf359e72856906
Naasir Bush <naasirbush@icloud.com>
2022-08-13 06:47:31 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ScotchInfoBar from './ScotchInfoBar';
+import './styles.css';
+
+function App() {
+  const apiURL = 'https://www.anapioficeandfire.com/api/books?pageSize=30';
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button">Fetch Data</button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+
+      {/* Use JSX below for each book */}
+      <div className="books">
+        <div className="book">
+          <h3>Book Number</h3>
+          <h2>Book Name</h2>
+
+          <div className="details">
+            <p>👨: Author/Authors</p>
+            <p>📖: Number of pages</p>
+            <p>🏘️: Book Country</p>
+            <p>⏰: Release date</p>
+          </div>
+        </div>
+      </div>
+
+      <ScotchInfoBar seriesNumber="7" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
